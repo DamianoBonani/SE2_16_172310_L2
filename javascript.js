d64e886a4f046c00011970ca32bc2e1a1fba3a48
damianobonani <damiano.bonani@studenti.unitn.it>
2016-11-14 19:37:20 +0100

    Aggiunta della funzione per la comparsa e la scomparsa dei campi di input dell'item e della quantita

--- javascript.js
+++ javascript.js
@@ -1,3 +1,15 @@
 /***************
 javascript
 ****************/
+
+var visibile = false; // indica se la parte di input è visibile
+/**
+* Funzione per la gestione della comparsa e scomparsa dei campi di input
+*/
+function controlloUploader(){
+	if(visibile)
+		document.getElementById("campiInserimento").className = "invisibile"; //rendo invisibile
+	else
+		document.getElementById("campiInserimento").className = "visibile"; // rendo visibile
+	visibile=!visibile;
+}
