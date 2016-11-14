bdb2895aaceb9503ba24ce2579c5c86070ec4031
damianobonani <damiano.bonani@studenti.unitn.it>
2016-11-14 19:42:44 +0100

    Aggiunta delle funzione per verificare su i campi di input dell'item e della quantita sono validi

--- javascript.js
+++ javascript.js
@@ -13,3 +13,39 @@ function controlloUploader(){
 		document.getElementById("campiInserimento").className = "visibile"; // rendo visibile
 	visibile=!visibile;
 }
+
+/**
+* Controlla se il campo di input dell'item è valido
+* @param {Object} oggetto che indica il rispettivo campo di input
+* @return true se è valido
+* @return false se non è valido
+*/
+function controlloItem(oggetto){
+	if(oggetto.value!="")
+		return true;
+	else
+		return false;	
+}
+
+/**
+* Controlla se la stringa inviata è un numero
+* @param {String} n indica la stringa da controllare
+* @return true se è un numero
+* @return false se non è un numero
+*/
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+/**
+* Controlla se il campo di input della quantità è valido
+* @param {Object} oggetto che indica il rispettivo campo di input
+* @return true se è valido
+* @return false se non è valido
+*/
+function controlloQuantita(oggetto){
+	if(oggetto.value!="" && isNumeric(oggetto.value))
+		return true;
+	else
+		return false;	
+}
